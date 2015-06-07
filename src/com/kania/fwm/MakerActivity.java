a8a5a817d8d38f7c6f878161a0b63987522c22ad
Dal-Pi <kimsh707@naver.com>
2015-06-08 02:25:01 +0900

    [150608] (test) change layout name

--- src/com/kania/fwm/MakerActivity.java
+++ src/com/kania/fwm/MakerActivity.java
@@ -10,6 +10,6 @@ public class MakerActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_maker);
 	}
 }
