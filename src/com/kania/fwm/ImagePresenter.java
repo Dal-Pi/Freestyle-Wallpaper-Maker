b68df9b39274378afdb85c3af8c668cfc175452b
Dal-Pi <kimsh707@naver.com>
2015-06-13 04:11:09 +0900

    [150613] add functions
    implement adding image items
    divided layout to button layout and inner layout used to add image items
    add permission for set wallpapar

--- src/com/kania/fwm/ImagePresenter.java
+++ src/com/kania/fwm/ImagePresenter.java
@@ -1,34 +1,90 @@
 package com.kania.fwm;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import android.app.Activity;
 import android.content.Context;
-import android.util.Log;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore.Images;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Toast;
 
 public class ImagePresenter {
 	
-	private Context mContext;
-	private ViewGroup mRootView;
+	private Activity mContext;
+	private ViewGroup mlayoutItmes;
 	
-	public ImagePresenter(Context context, ViewGroup vg) {
+//	private ArrayList<RelativeLayout> mImageList;
+	
+	public ImagePresenter(Activity context, ViewGroup vg) {
 		mContext = context;
-		mRootView = vg;
+		mlayoutItmes = vg;
+//		mImageList = new ArrayList<RelativeLayout>();
+	}
+	
+	public void getImageFromGallery() {
+		Intent intent = new Intent();
+		intent.setAction(Intent.ACTION_GET_CONTENT);
+		intent.setType("image/*");
+		mContext.startActivityForResult(intent, MakerActivity.REQ_CODE_GALLERY);
+	}
+	
+	public void addNewItemToMaker(Uri uri) {
+		try {
+			Bitmap bitmap = Images.Media.getBitmap(mContext.getContentResolver(), uri);
+			if (bitmap != null) {
+				LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				RelativeLayout layout = (RelativeLayout) inflater.inflate(R.layout.layout_item, null);
+				ImageView image = new ImageView(mContext);
+				image.setImageBitmap(bitmap);
+				layout.addView(image);
+				layout.setOnTouchListener(getImageTouchListener());
+				mlayoutItmes.addView(layout);
+			}
+			
+		} catch (FileNotFoundException e) {
+			Toast.makeText(mContext, "Cannot find file : " + uri.getPath(), Toast.LENGTH_SHORT).show();
+			e.printStackTrace();
+		} catch (IOException e) {
+			Toast.makeText(mContext, "Occured an error to load image", Toast.LENGTH_SHORT).show();
+			e.printStackTrace();
+		}
 	}
 	
 	private View.OnTouchListener mUpdownMovableTouchListener = new View.OnTouchListener() {
+		public int savedY = 0;
 		@Override
 		public boolean onTouch(View v, MotionEvent event) {
 			int action = event.getAction();
 			int y = (int) event.getRawY();
 			
 			switch (action) {
+			case MotionEvent.ACTION_DOWN:
+				savedY = y;
+				v.setPressed(true);
+				return true;
 			case MotionEvent.ACTION_MOVE:
 				int[] origin = new int[2];
 				v.getLocationInWindow(origin);
 				int width = v.getWidth();
 				int height = v.getHeight();
 				v.layout(origin[0], y-height/2, origin[0]+width, y+height/2);
+				return true;
+			case MotionEvent.ACTION_UP:
+				//TODO needed to change using timer.
+				int  viewHeight = v.getHeight();
+				if (Math.abs(savedY - y) < viewHeight/2) {
+					v.setPressed(false);
+					v.performClick();
+				}
 				return true;
 			}
 			return true;
@@ -38,17 +94,65 @@ public class ImagePresenter {
 	private View.OnTouchListener mMoveAndExpendableTouchListener = new View.OnTouchListener() {
 		@Override
 		public boolean onTouch(View v, MotionEvent event) {
+			int action = event.getAction();
+			int x = (int) event.getRawX();
+			int y = (int) event.getRawY();
 			
+			//TODO needed to implement 2 point touch that can resize images.
 			
+			switch (action) {
+			case MotionEvent.ACTION_MOVE:
+				int[] origin = new int[2];
+				v.getLocationInWindow(origin);
+				int width = v.getWidth();
+				int height = v.getHeight();
+				v.layout(x-width/2, y-height/2, x+width/2, y+height/2);
+				return true;
+			}
 			return true;
 		}
 	};
 	
-	public View.OnTouchListener getButtonListener() {
+	private View.OnClickListener mAddbtnCilckListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			getImageFromGallery();
+		}
+	};
+	
+	private View.OnClickListener mSetbtnCilckListener = new View.OnClickListener() {
+		@Override
+		public void onClick(View v) {
+			mlayoutItmes.setDrawingCacheEnabled(true);
+			Bitmap wallpaper = mlayoutItmes.getDrawingCache();
+			//TODO with setting image to wallpaper, save image to file for user set it to lockscreen image.
+			if (wallpaper != null) {
+				try {
+					//TODO try to using wallpapermanager
+					mContext.setWallpaper(wallpaper);
+					mContext.finish();
+				} catch (IOException e) {
+					Toast.makeText(mContext, "Occured an error to set image as wallpaper", Toast.LENGTH_SHORT).show();
+					e.printStackTrace();
+				}
+			}
+			
+		}
+	};
+	
+	public View.OnTouchListener getButtonTouchListener() {
 		return mUpdownMovableTouchListener;
 	}
 	
-	public View.OnTouchListener getImageListener() {
+	public View.OnTouchListener getImageTouchListener() {
 		return mMoveAndExpendableTouchListener;
+	}
+	
+	public View.OnClickListener getAddbtnClickListener() {
+		return mAddbtnCilckListener;
+	}
+	
+	public View.OnClickListener getSetbtnClickListener() {
+		return mSetbtnCilckListener;
 	}
 }
--- src/com/kania/fwm/MakerActivity.java
+++ src/com/kania/fwm/MakerActivity.java
@@ -1,18 +1,21 @@
 package com.kania.fwm;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 public class MakerActivity extends Activity{
 	RelativeLayout layoutMaker;
+	RelativeLayout layoutItems;
 	ImageButton mImgbtnAdd, mImgbtnSet;
 	
 	ImagePresenter presenter;
+	
+	public static final int REQ_CODE_GALLERY = 10000;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -22,11 +25,14 @@ public class MakerActivity extends Activity{
 		layoutMaker = (RelativeLayout) findViewById(R.id.layoutMaker);
 		mImgbtnAdd = (ImageButton) findViewById(R.id.imgbtnAddImage);
 		mImgbtnSet = (ImageButton) findViewById(R.id.imgbtnSetWallpaper);
+		layoutItems = (RelativeLayout) findViewById(R.id.layoutItems);
 		
-		presenter = new ImagePresenter(this, layoutMaker);
+		presenter = new ImagePresenter(this, layoutItems);
 		
-		mImgbtnAdd.setOnTouchListener(presenter.getButtonListener());
-		mImgbtnSet.setOnTouchListener(presenter.getButtonListener());
+		mImgbtnAdd.setOnTouchListener(presenter.getButtonTouchListener());
+		mImgbtnAdd.setOnClickListener(presenter.getAddbtnClickListener());
+		mImgbtnSet.setOnTouchListener(presenter.getButtonTouchListener());
+		mImgbtnSet.setOnClickListener(presenter.getSetbtnClickListener());
 	}
 	
 	@Override
@@ -37,4 +43,20 @@ public class MakerActivity extends Activity{
 				View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
 		super.onResume();
 	}
+	
+	
+	
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		switch (requestCode) {
+		case REQ_CODE_GALLERY:
+			if (data != null) {
+				Uri imgUri = data.getData();
+				presenter.addNewItemToMaker(imgUri);
+			}
+			break;
+		}
+		super.onActivityResult(requestCode, resultCode, data);
+	}
+	
 }
